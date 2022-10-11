1dc4b7f2b08de8ee8313e010802a27bcb927623f
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 04:03:10 +0000

    7lab1

--- main.py
+++ main.py
@@ -3,10 +3,8 @@
 # Make your Code
 # ******************************
 numbers = list(map(int, input().split()))
-max_ = max(numbers)
-min_ = min(numbers)
-numbers.remove(max_)
-numbers.remove(min_)
+numbers.remove(max)
+numbers.remove(min)
 sum = 0
 for i in range(len(numbers)):
     sum += numbers[i]
